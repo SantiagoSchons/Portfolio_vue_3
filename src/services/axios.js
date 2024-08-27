c01d1369bedca301a4078c7005178a86e3334d2b
SantiagoSchons <santiagodeoliveira2004@gmail.com>
2024-08-27 08:56:19 -0300

    deployment dokku

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-    baseURL: 'http://172.17.0.3:5000',
+    baseURL: 'http://192.168.0.113:46071',
     withCredentials: false,
     headers: {
       Accept: 'application/json',
