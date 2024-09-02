76ff562c88b6388bb09bf7205c3eae168f903e5f
SantiagoSchons <santiagodeoliveira2004@gmail.com>
2024-09-02 12:25:20 -0300

    ip

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,16 +1,16 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-    baseURL: 'http://192.168.0.113:8080',
-    withCredentials: false,
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    }
+  baseURL: 'http://191.52.62:8080',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
 })
 
 export default {
-    getEvents() {
-      return apiClient.get('/certificacoes/')
-    }
+  getEvents() {
+    return apiClient.get('/certificacoes/')
+  }
 }
